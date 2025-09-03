83839f484528ae8bd823f8d7cb177236d212351d
Difa Nugraha <123245678+DifaaNug@users.noreply.github.com>
2025-09-03 12:57:13 +0700

    Menambahkan Route Resource Posts untuk Halaman web
    Mendaftarkan Route Resource Posts untuk halaman web , menambahkan middleware auth untuk keamanan

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+
+//Mendaftarkan Route Halaman Web
+Route::resource('posts', \App\Http\Controllers\PostController::class)->middleware(['auth']);
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
