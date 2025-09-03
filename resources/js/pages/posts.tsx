609b54bc8e9eae4dc6d6a0cef3f5090b2f66dae3
Difa Nugraha <123245678+DifaaNug@users.noreply.github.com>
2025-09-03 13:13:26 +0700

    Menambahkan Posts page dan PostFormModal Componen
    Membuat Halaman Post Page Baru serta Menambahkan Componen PostFromModal untuk isi nya

---
+++ resources/js/pages/posts.tsx
@@ -0,0 +1,21 @@
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Posts',
+        href: '/Posts',
+    },
+];
+
+export default function Posts() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Posts" />
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                <p>Ini Halaman Post</p>
+            </div>
+        </AppLayout>
+    );
+}
